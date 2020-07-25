00e2f6412725efc882dcfb99fa910106dac624a0
Guilherme FGR <Guifgr@hotmail.com>
2020-07-24 23:06:11 -0300

    0.01

--- php/connect.php
+++ php/connect.php
@@ -1,11 +1,11 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
+$servername = "127.0.0.1:3306";
+$username = "u754421247_demo";
+$password = "Nubhue040499";
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=demo", $username, $password);
+  $conn = new PDO("mysql:host=$servername;dbname=u754421247_demo", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
