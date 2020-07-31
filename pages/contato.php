59016982140af71e16c9c36d86aba1287c4eea85
Guilherme FGR <Guifgr@hotmail.com>
2020-07-30 23:03:34 -0300

    0.01

--- pages/contato.php
+++ pages/contato.php
@@ -12,7 +12,7 @@
         <h2><?php echo $nome1?></h2>
         <p class="title"><?php echo $cargo1?></p>
         <p><?php echo $texto1?></p>
-        <p><a class="email" href="mailto:<?php echo $email2?>"><?php echo $email1?></a></p>
+        <p><a target="_top" class="email" href="mailto:<?php echo $email1?>"><?php echo $email1?></a></p>
         <a href="<?php echo $link1?>"><p><button class="button">Contato</button></p></a>
       </div>
     </div>
@@ -25,7 +25,7 @@
         <h2><?php echo $nome2?></h2>
         <p class="title"><?php echo $cargo2?></p>
         <p><?php echo $texto2?></p>
-        <p><a class="email" href="mailto:<?php echo $email2?>"><?php echo $email2?></a></p>
+        <p><a target="_top" class="email" href="mailto:<?php echo $email2?>"><?php echo $email2?></a></p>
         <a href="<?php echo $link2?>"><p><button class="button">Contato</button></p></a>
       </div>
     </div>
@@ -38,7 +38,7 @@
         <h2><?php echo $nome3?></h2>
         <p class="title"><?php echo $cargo3?></p>
         <p><?php echo $texto3?></p>
-        <p><a class="email" href="<?php echo $email3?>"><?php echo $email3?></a></p>
+        <p><a target="_top" class="email" href="<?php echo $email3?>"><?php echo $email3?></a></p>
         <a href="mailto:<?php echo $link3?>"><p><button class="button">Contato</button></p></a>
       </div>
     </div>
